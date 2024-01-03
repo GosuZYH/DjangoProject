58e14a088d3ab2ecfff062c09ffc60a3ea0fa0d6
张宇航 <2657955369@qq.com>
2024-01-03 16:06:11 +0800

    fix: refactor project's file-stack.

--- MyDjango/__init__.py
+++ MyDjango/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-# _*_ coding: utf-8 _*_
-#
-# Copyright (C) 2023 - 2023 Gosu, Inc. All Rights Reserved 
-#
-# @Time    : 2023/12/12 13:55
-# @Author  : GosuXX
-# @File    : __init__.py.py
+import pymysql
+
+pymysql.install_as_MySQLdb()
